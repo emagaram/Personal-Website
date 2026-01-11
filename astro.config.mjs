07924333e71a2a19d59269aa328054a59e6af047
Ezra M <ezragithub.truth260@simplelogin.fr>
2026-01-10 18:47:22 -0800

    build: switch to vercal image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({ imageService: true }),
   vite: {
     plugins: [tailwindcss()],
   },
