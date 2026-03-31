1559e43bdebfa71bf0f98be5d3c96ec2c563d555
Ezra M <ezragithub.truth260@simplelogin.fr>
2026-03-30 20:58:52 -0700

    refactor: content updates

--- src/components/home/Skills/SkillDescriptions/React.tsx
+++ src/components/home/Skills/SkillDescriptions/React.tsx
@@ -2,7 +2,7 @@ export default function React() {
     return <div>
         <p className="text-base">
             This modal component and animation was built using React with the Motion library. I've been working with React since 2020, and it's become one of my core strengths.
-            Some of the react libraries I've used are Next.js, React Query, and Zustand, along with UI
+            Some of the react libraries I've used are Next.js, TanStack Query, and Zustand, along with UI
             libraries including Shadcn, Radix UI, and Material UI.
         </p>
         <br />
